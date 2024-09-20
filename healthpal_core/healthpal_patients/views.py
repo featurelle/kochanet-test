bac9588aff710ff7127d77fe11b676b2cf1fa3f8
featurelle <s3m.channel@gmail.com>
2024-09-20 16:15:36 +0000

    views for patients

--- healthpal_core/healthpal_patients/views.py
+++ healthpal_core/healthpal_patients/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Patient
+from .serializers import PatientSerializer
+
+
+class PatientListCreateView(generics.ListCreateAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+class PatientRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
