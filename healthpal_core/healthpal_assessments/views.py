b9710ba955f54eb0d4a00ca8d06a027acd815fd0
featurelle <s3m.channel@gmail.com>
2024-09-22 08:29:04 +0000

    added filtering and ordering to listing

--- healthpal_core/healthpal_assessments/views.py
+++ healthpal_core/healthpal_assessments/views.py
@@ -1,11 +1,47 @@
 from rest_framework import generics
-from .models import PatientAssessment
-from .serializers import PatientAssessmentSerializer
+from rest_framework.filters import OrderingFilter
+
+from django_filters import rest_framework as filters
+from rest_framework.pagination import PageNumberPagination
+
+from healthpal_assessments.models import PatientAssessment
+from healthpal_assessments.serializers import PatientAssessmentSerializer
 
 
+# Utility classes
+class PatientAssessmentFilter(filters.FilterSet):
+    """
+    Filtering class to filter PatientAssessments, usage in ListView:
+        `filter_backends = (filters.DjangoFilterBackend, ...)`
+        `filterset_class = PatientAssessmentFilter`
+    """
+    type = filters.CharFilter(field_name='type')
+    date = filters.DateFilter(field_name='date')
+    date_gte = filters.DateFilter(field_name='date', lookup_expr='gte')
+    date_lte = filters.DateFilter(field_name='date', lookup_expr='lte')
+    patient = filters.NumberFilter(field_name='patient__id')
+
+    class Meta:
+        model = PatientAssessment
+        fields = ['type', 'date', 'patient']
+
+
+class SmallResultsSetPagination(PageNumberPagination):
+    """Pagination class setup for page size from 5 up to 100"""
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+# Views
 class PatientAssessmentListCreateView(generics.ListCreateAPIView):
     queryset = PatientAssessment.objects.all()
     serializer_class = PatientAssessmentSerializer
+    filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
+    filterset_class = PatientAssessmentFilter
+    pagination_class = SmallResultsSetPagination
+    ordering_fields = ['date', 'type', 'patient']
+    ordering = ['date']  # Default ordering
 
 
 class PatientAssessmentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
