2be6c214d9816ee6c25e02b97bc16bce8c09114a
featurelle <s3m.channel@gmail.com>
2024-09-22 16:15:43 +0000

    got new custom validations into account

--- healthpal_core/healthpal_assessments/serializers.py
+++ healthpal_core/healthpal_assessments/serializers.py
@@ -1,6 +1,8 @@
+from django.core.exceptions import ValidationError as ModelValidationError
 from rest_framework import serializers
 
-from .models import PatientAssessment, AssessmentQnARound
+from healthpal_patients.models import Patient
+from .models import PatientAssessment, AssessmentQnARound, validate_assessment_date_gte_patients_birthdate
 
 
 class QnARoundSerializer(serializers.ModelSerializer):
@@ -10,11 +12,30 @@ class QnARoundSerializer(serializers.ModelSerializer):
 
 
 class PatientAssessmentSerializer(serializers.ModelSerializer):
-    qna_rounds = QnARoundSerializer(many=True, required=False)
+    qna_rounds = QnARoundSerializer(many=True, required=True)
 
     class Meta:
         model = PatientAssessment
         fields = '__all__'
+
+    def validate_date(self, value):
+        patient_id = self.get_initial().get('patient')
+
+        if not patient_id or not isinstance(patient_id, int):
+            return value  # Skip validation if no valid patient_id provided
+
+        try:
+            patient = Patient.objects.get(id=patient_id)
+        except (Patient.DoesNotExist, ValueError):
+            return value  # Skip validation if patient does not exist or ID is invalid
+
+        # Perform validation for the assessment date being >= patient's birthdate
+        try:
+            validate_assessment_date_gte_patients_birthdate(value, patient.birthdate)
+        except ModelValidationError as e:
+            raise serializers.ValidationError(e.messages)
+
+        return value
 
     def create(self, validated_data):
         qna_rounds_data = validated_data.pop('qna_rounds', [])
